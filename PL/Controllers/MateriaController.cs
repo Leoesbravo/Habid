8cfaf6d61ca14a5670750e52c0e7bd8ace1918fd
Leoesbravo <lescogidobravo@outlook.com>
2023-01-12 11:50:17 -0600

    template ajustado

---
+++ PL/Controllers/MateriaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class MateriaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
